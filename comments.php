752490ffdac7c474dedc3904e299ec037757fe4e
hafizal hussin <hafizalhussin@hafizals-MacBook-Pro.local>
2020-03-09 11:11:55 +0800

    new:comments

---
+++ comments.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <h1> Laman PHP</h1>
+        <?php
+        // echo "Hello";
+        //This is single comments
+        /*
+        this is multiple line
+        comments
+        */
+        ?>
+    </body>
+</html>
