977b8d12f4e69b763e52fd50844c4360e5bbda36
hafizal hussin <hafizalhussin@hafizals-MacBook-Pro.local>
2020-03-09 12:21:57 +0800

    new:display variables

---
+++ display.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    //display text
+    echo "<h1> DISPLAY TEXT</h1>";
+    echo "<h2> Selamat Datang Ke ICT JOHOR </h2>";
+    echo "Kota Iskandar <br>";
+    
+    //display variables
+    echo "<h1> Display Variables </h1>";
+    $txt1 = "Selamat Datang Ke ICT JOHOR";
+    $txt2 = "Kota Iskandar";
+
+    echo "<h2>" . $txt1 . "</h2>";
+    echo "<p>" . $txt2 . "</p>";
+
+    ?>
+</body>
+</html>
